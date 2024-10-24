7d57ed7bbb5b351b541cc40020b6ac6a2368f919
Okkar25 <okkaaung2508@gmail.com>
2024-10-24 19:44:06 +0630

    JavaScript POST Request

--- 4_form_req_file.py
+++ 4_form_req_file.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import (
+    Flask,
+    render_template,
+    request,
+    session,
+    redirect,
+    url_for,
+    Response,
+    send_from_directory,
+    jsonify,
+)
 import pandas as pd
+import os
+import uuid
 
 app = Flask(__name__, template_folder="templates")
 
@@ -34,7 +46,9 @@ def file_upload():
         or file.content_type == "application/vnd.ms-excel"
     ):
         data_frame = pd.read_excel(file)
-        table_html = data_frame.to_html(classes="wide-table", index=False)
+        table_html = data_frame.to_html(
+            classes="wide-table", index=False
+        )
         custom_css = """
         <style>
         .wide-table {
@@ -54,6 +68,68 @@ def file_upload():
 
     return (
         "<h2>Unsupported file type. Please upload a xlsx/xls or plain text file.</h2>"
+    )
+
+
+STATIC_FOLDER = os.path.join(app.root_path, "static", "downloads")
+
+if not os.path.exists(STATIC_FOLDER):
+    os.makedirs(STATIC_FOLDER)
+
+
+@app.route("/convert_csv", methods=["POST"])
+def convert_csv():
+    file = request.files["file"]
+
+    df = pd.read_excel(file)
+    # csv_file_path = os.path.join(STATIC_FOLDER, "result.csv")
+    # df.to_csv(csv_file_path, index=False, encoding="utf-8")
+
+    # return f"<h2>File successfully saved! <a href='/static/downloads/result.csv' download>Click here to download</a> </h2>"
+
+    response = Response(
+        df.to_csv(index=False, encoding="utf-8"),
+        mimetype="text/csv",
+        headers={"Content-Disposition": "attachment; filename = result.csv"},
+    )
+
+    return response
+
+
+@app.route("/convert_csv_two", methods=["POST"])
+def convert_csv_two():
+    file = request.files["file"]
+    df = pd.read_excel(file)
+
+    if not os.path.exists("downloads"):
+        os.makedirs("downloads")
+
+    filename = f"{uuid.uuid4()}.csv"
+
+    df.to_csv(os.path.join("downloads", filename), index=False, encoding="utf-8")
+
+    return render_template("download.html", filename=filename)
+
+
+@app.route("/download/<filename>")
+def download(filename):
+    return send_from_directory("downloads", filename, download_name="result.csv")
+
+
+@app.route("/handle_post", methods=["POST"])
+def handle_post():
+    greeting = request.json["greeting"]
+    name = request.json.get("name")
+
+    with open("file.txt", "w") as file:
+        file.write(f"{greeting}, {name}!")
+
+    return jsonify(
+        # response data
+        {
+            "message": "Successfully written!",
+            "received_data": {"greeting": greeting, "name": name},
+        }
     )
 
 
